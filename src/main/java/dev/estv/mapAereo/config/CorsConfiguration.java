a9ad99043bd0f8875868fa6cc2db8e69877dfd87
estevao081 <thiagoestevao3030@gmail.com>
2025-07-17 10:58:07 -0300

    CORS config updated

--- src/main/java/dev/estv/mapAereo/config/CorsConfiguration.java
+++ src/main/java/dev/estv/mapAereo/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://mapaereo.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }

afeed216eed1071e8f88aadd5fb1a0af3b929ae3
Thiago <thiagoestevao3030@gmail.com>
2025-07-30 02:19:52 -0300

    date validation adjusted

--- src/main/java/dev/estv/mapAereo/services/ProductService.java
+++ src/main/java/dev/estv/mapAereo/services/ProductService.java
@@ -66,7 +66,8 @@ public class ProductService {
 
     public void validateDate(String date) {
         if (!date.matches("^(?:(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}|null)?$")
-                || !isValidDate(date)) {
+                || !isValidDate(date)
+                || !date.isBlank()) {
             throw new InvalidDateException();
         }
     }

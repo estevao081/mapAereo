69908bd64d182d6754f1ceac61a4fb61b726f6d6
Thiago <thiagoestevao3030@gmail.com>
2025-07-30 02:05:10 -0300

    deploy test

--- src/main/java/dev/estv/mapAereo/services/ProductService.java
+++ src/main/java/dev/estv/mapAereo/services/ProductService.java
@@ -65,11 +65,7 @@ public class ProductService {
     }
 
     public void validateDate(String date) {
-<<<<<<< HEAD
         if (!date.matches("^(?:(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}|null)?$")
-=======
-        if (!date.matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[0-9]{4}$")
->>>>>>> 47f8455bdf0949cf008261cea416dc6c67ff9ebe
                 || !isValidDate(date)) {
             throw new InvalidDateException();
         }
